b421c6ca90d23f6dbc82781c3c90b47cf0f77e80
Ibrahim Sifat <ibsifat24@gmail.com>
2022-05-18 16:06:22 +0300

    createpost

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require("path");
+// file upload schema
+const BlogPost = require("./models/BlogPost");
+//image upload
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
 
 //connecting mongodb
 const mongoose = require("mongoose");
@@ -11,11 +16,24 @@ mongoose.connect("mongodb://localhost/my_database", { useNewUrlParser: true });
 //use static file
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //routes
 
 //setting routers
+app.get("/", async (req, res) => {
+  const blogposts = await BlogPost.find({});
+  res.render("index", {
+    blogposts,
+  });
+});
 app.get("/", (req, res) => {
   // res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  console.log(req.params.id);
   res.render("index");
 });
 app.get("/about", (req, res) => {
@@ -35,9 +53,16 @@ app.get("/posts/new", (req, res) => {
 });
 
 //post a post
-app.post("/posts/store", (req, res) => {
-  console.log(req.body);
+app.post("/posts/store", async (req, res) => {
+  await BlogPost.create(req.body);
   res.redirect("/");
+});
+// find post
+app.get("/post/:id", async (req, res) => {
+  const blogpost = await BlogPost.findById(req.params.id);
+  res.render("post", {
+    blogpost,
+  });
 });
 
 app.listen(3000, () => {
