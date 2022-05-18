4d8de1c671d64a24d1898a90ea0769424d3200fe
Ibrahim Sifat <ibsifat24@gmail.com>
2022-05-18 14:41:53 +0300

    postApost

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express();
 const ejs = require("ejs");
 const path = require("path");
 
+//connecting mongodb
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/my_database", { useNewUrlParser: true });
+
+// setting template engine
 //use static file
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 //routes
-
-// setting template engine
 
 //setting routers
 app.get("/", (req, res) => {
@@ -27,6 +30,16 @@ app.get("/post", (req, res) => {
   // res.sendFile(path.resolve(__dirname, "pages/post.html"));
   res.render("post");
 });
+app.get("/posts/new", (req, res) => {
+  res.render("create");
+});
+
+//post a post
+app.post("/posts/store", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
 app.listen(3000, () => {
   console.log(`listing port 3000`);
 });
